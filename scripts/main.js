362b8dbb988b97dc0c5bbe1d31a061e63fa28d51
kitsumori <kitsumori444@gmail.com>
2020-06-14 06:36:31 -0500

    Midway through -before changing message from firm

--- scripts/main.js
+++ scripts/main.js
@@ -2,39 +2,10 @@ let myImage = document.querySelector('img');
 
 myImage.onclick = function(){
     let mySrc = myImage.getAttribute('src');
-    if(mySrc === 'images/pekora.png'){
-        myImage.setAttribute ('src','images/pekora-drunk.png');
+    if(mySrc === 'images/forest.png'){
+        myImage.setAttribute ('src','images/forest2.jpg');
     } else {
-        myImage.setAttribute ('src', 'images/pekora.png');
-    }
-    
-}
-
-//setting variables
-let myButton = document.querySelector('button');
-let myHeading = document.querySelector('h1');
-
-//function
-function setUsername(){
-    let myName = prompt('Please enter your name-ぺこ。');
-    if(!myName || myName === null){
-        setUsername();
-    } else {
-
-    localStorage.setItem('name', myName);
-    myHeading.textContent = 'Welcomew, ' + myName + '-様';
+        myImage.setAttribute ('src', 'images/forest.png');
     }
 
 }
-
-if (!localStorage.getItem('name')) {
-    setUsername();
-} else {
-    let storedName = localStorage.getItem('name');
-    myHeading.textContent = 'Welcomew, ' + storedName + '-様';
-}
-
-myButton.onclick = function(){
-    setUsername();
-}
-
